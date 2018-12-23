89522988b5cf02678acf6b2a819358944ec4dfcd
WangASong-1 <1485728127@qq.com>
2018-12-23 20:27:50 +0800

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    Dictionary<System.Object, int> abc = new Dictionary<System.Object, int>();
+
+	// Use this for initialization
+	void Start () {
+        abc["abc"] = 1;
+        abc["abc"] = 2;
+        foreach (var item in abc)
+        {
+            Debug.Log("key = " + item.Key);
+            Debug.Log("value = " + item.Value);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
